eeebac94d84b96d11be3feda63cc368416ff4557
Daniel <daniel@cellkey.co.kr>
2024-11-27 14:41:41 +0900

    Initial commit

---
+++ src/test/java/net/danal/batch/BatchGovernmentGeneralRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package net.danal.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchGovernmentGeneralRestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
